ddcb90cf68e2a0a8486f292b737de7cf014f85e5
Vladislav <139064573+worlems@users.noreply.github.com>
2025-01-24 20:46:08 +0200

    add comment and js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const modal = document.querySelector('.backdrop'); 
+const closeModalBtn = document.querySelector('.modal-close-btn'); 
+
+// Добавляем событие клика для кнопки
+closeModalBtn
+closeModalBtn
+
+close
+addEventListener('click', () => {
+  modal.
+  
+classList.remove('is-open'); // Убираем класс и скрываем окно
+});
