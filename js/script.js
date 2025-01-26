9b303983242a83fd22a277aa8894feab691a80df
Vladislav <139064573+worlems@users.noreply.github.com>
2025-01-26 03:24:05 +0200

    hw 6

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-const modal = document.querySelector('.backdrop'); 
-const closeModalBtn = document.querySelector('.modal-close-btn'); 
+// const modal = document.querySelector('.backdrop'); 
+// const closeModalBtn = document.querySelector('.modal-close-btn'); 
 
-// Добавляем событие клика для кнопки
-closeModalBtn
-closeModalBtn
+// // Добавляем событие клика для кнопки
+// closeModalBtn
+// closeModalBtn
 
-close
-addEventListener('click', () => {
-  modal.
+// close
+// addEventListener('click', () => {
+//   modal.
   
-classList.remove('is-open'); // Убираем класс и скрываем окно
+// classList.remove('is-open'); // Убираем класс и скрываем окно
+// });
+
+const menuBtns = document.querySelectorAll('.modal-close-btn');
+const menuEl = document.querySelector('.backdrop');
+
+menuBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    menuEl.classList.toggle('is-open');
+  });
 });
